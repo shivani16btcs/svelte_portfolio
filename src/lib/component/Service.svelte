dd83b9d679ed0b5fed74ef6c70583c0bc0148cc0
shivani16btcs <shivani16btcs0413@gmail.com>
2023-09-30 16:13:31 +0530

    services added and theam changes

---
+++ src/lib/component/Service.svelte
@@ -0,0 +1,45 @@
+<!-- Services -->
+<section class="services" id="services">
+    <div class="max-width">
+        <h2 class="title">My services</h2>
+        <div class="serv-content">
+            <div class="card">
+                <div class="box">
+                    <i class="fas fa-code"></i>
+                    <div class="text">Web Development</div>
+                    <p>
+                        Proficient in building user-friendly web applications
+                    </p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="box">
+                    <i class="fas fa-laptop-code"></i>
+                    <div class="text">Full-Stack Expertise</div>
+                    <p>
+                        Experienced in creating seamless user experiences from server to client
+                    </p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="box">
+                    <i class="fas fa-user-secret"></i>
+                    <div class="text">Innovation & Optimization</div>
+                    <p>
+                        Innovative thinker with a track record of tackling complex technical challenges
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<style>
+    .services{
+      padding:80px 0;
+    }
+  </style>
+  
+  
+  
