e8cc9226c3e2492e230978dbcd665d324ac452ce
shivani16btcs <shivani16btcs0413@gmail.com>
2023-10-07 15:46:20 +0530

    stable code

--- src/lib/component/skills/Skill.svelte
+++ src/lib/component/skills/Skill.svelte
@@ -3,19 +3,9 @@
 </script>
 
 
-<section class="skills skill-head" id="skills">
+<section class="skills skill-head overflow-hidden" id="skills">
 	<div class="max-width">
 		<h2 class="title">My Skills</h2>
-		<p
-			class="{true
-				? 'opacity-100 transform translate-y-0 transition duration-500'
-				: 'opacity-0 transform -translate-y-4 transition duration-300'} text-left mx-4 mt-6"
-		>
-			I am well-versed in a wide range of web development technologies and tools, and I work with
-			them daily to create seamless and engaging online experiences. My expertise includes:
-		</p>
-		
-		
 	</div>
 	<div class="relative skill-content flex justify-center pb-32 mt-96 lg:mt-0  md:mb-10 lg:mb-20 mb-40">
 		<div class="abolute z-0 ">
