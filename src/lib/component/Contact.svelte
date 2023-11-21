00ad2654d538540925958731d828959006bee9bf
shivani16btcs <shivani16btcs0413@gmail.com>
2023-11-21 23:49:51 +0530

    contact us updates

--- src/lib/component/Contact.svelte
+++ src/lib/component/Contact.svelte
@@ -1,4 +1,5 @@
 <script>
+	import contactUSIMG from '$lib/images/contact_us_gif_tranparent_bg.gif';
 	let status = '';
 	let isContactFormSubmitted = false;
 	let isLoading = false;
@@ -32,59 +33,12 @@
 		<h2 class="title">Contact</h2>
 		<div class="contact-content">
 			<div class="column left">
-				<div class="text">Get in Touch</div>
+				<!-- <div class="text">Get in Touch</div>
 				<p>
 					Feel free to get in touch with me. I'll be happy to answer your questions and set up a
 					meeting with you. Reach out and let's create something amazing together!
-				</p>
-				<!-- <div class="info mt-1">
-					<div class="font-semibold">Language Known</div>
-					<div class="sub-title">Hindi, English</div>
-				</div> -->
-				<div class="mt-2">
-					<!-- Social -->
-					<a href="https://www.linkedin.com/in/shivani-yadav-abc/" class="mr-2 contact-social-icon">
-						<button
-							type="button"
-							data-te-ripple-init
-							data-te-ripple-color="light"
-							class="hover:animate-bounce mb-2 inline-block rounded-full px-3 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
-							style="background-color: #1e3551"
-						>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								class="h-3 w-3"
-								fill="currentColor"
-								viewBox="0 0 24 24"
-							>
-								<path
-									d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z"
-								/>
-							</svg>
-						</button>
-					</a>
-
-					<a href="https://github.com/shivani16btcs" class="contact-social-icon">
-						<button
-							type="button"
-							data-te-ripple-init
-							data-te-ripple-color="light"
-							class="hover:animate-bounce mb-2 inline-block rounded-full px-3 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
-							style="background-color: #1e3551"
-						>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								class="h-3 w-3"
-								fill="currentColor"
-								viewBox="0 0 24 24"
-							>
-								<path
-									d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
-								/>
-							</svg>
-						</button>
-					</a>
-				</div>
+				</p> -->
+				<img src={contactUSIMG} alt="Girl"  />
 			</div>
 
 			<!-- Reach -->
